5d436d8c2734f85a2507ac1383b560d537da1707
mojtabakhoja3teh@gmail.com <mojtabakhoja3teh@gmail.com>
2022-02-17 10:40:17 +0330

    Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,31 @@
+import React, { Fragment } from "react";
+import "../Header.css";
+
+const Header = () => {
+  return (
+    <Fragment>
+      <div className="header">
+        <div className="container-fluid">
+          <div className="row height-max align-items-center">
+            <div className="text-right pr-5 ml-auto col-md-9">
+              <h5 className="title-heading d-inline-block p-2 text-uppercase">
+                the all new
+              </h5>
+              <h2 className="text-uppercase my-2 title">2022 mercedes-benz</h2>
+              <h3 className="text-uppercase">model name</h3>
+              <h6 className="text-capitalize">starts from</h6>
+              <h2>
+                $60.000
+                <a href="a.html" className="title-icon d-inline-block">
+                  <i class="fa fa-play" aria-hidden="true"></i>
+                </a>
+              </h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
--- src/container/App.jsx
+++ src/container/App.jsx
@@ -1,11 +1,13 @@
 import React, { Fragment } from "react";
 import Navbar from "../components/Navbar";
+import Header from "../components/Header";
 import "../App.css";
 
 const App = () => {
   return (
     <Fragment>
       <Navbar />
+      <Header />
     </Fragment>
   );
 };
