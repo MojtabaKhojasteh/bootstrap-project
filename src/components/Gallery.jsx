bf58609d842ac3b15999e5cd858ad0be8e3a4086
mojtabakhoja3teh@gmail.com <mojtabakhoja3teh@gmail.com>
2022-03-04 18:03:06 +0330

    Gallery

---
+++ src/components/Gallery.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import "../Gallery.css";
+import carAmerican1 from "../img/car-american-1.jpeg";
+import carAmerican2 from "../img/car-american-2.jpeg";
+import carAmerican3 from "../img/car-american-3.jpeg";
+import carAmerican4 from "../img/car-american-4.jpeg";
+import carAmerican5 from "../img/car-american-5.jpeg";
+
+const Gallery = () => {
+  return (
+    <div className="gallery py-5">
+      <div className="container">
+        <div className="row mb-5">
+          <div className="col-10 d-flex flex-wrap mx-auto text-uppercase justify-content-center">
+            <h1 className="font-weight-bold align-self-center mx-1">Our</h1>
+            <h1 className="section-title-special mx-1">gallery</h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm-6 mx-auto">
+            <div className="gallery-item">
+              <img
+                className="img-fluid gallery-img"
+                src={carAmerican1}
+                alt="car"
+              />
+            </div>
+          </div>
+          <div className="col-sm-6 d-flex flex-column justify-content-between">
+            <div className="row">
+              <div className="col-sm-6">
+                <div className="gallery-item">
+                  <img
+                    className="img-fluid gallery-img"
+                    src={carAmerican2}
+                    alt=""
+                  />
+                </div>
+              </div>
+              <div className="col-sm-6">
+                <div className="gallery-item">
+                  <img
+                    className="img-fluid gallery-img"
+                    src={carAmerican3}
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-sm-6">
+                <div className="gallery-item">
+                  <img
+                    className="img-fluid gallery-img"
+                    src={carAmerican4}
+                    alt=""
+                  />
+                </div>
+              </div>
+              <div className="col-sm-6">
+                <div className="gallery-item">
+                  <img
+                    className="img-fluid gallery-img"
+                    src={carAmerican5}
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Gallery;
--- src/container/App.jsx
+++ src/container/App.jsx
@@ -6,6 +6,7 @@ import Skills from "../components/Skills";
 import Inventory from "../components/Inventory";
 import Question from "../components/Question";
 import Featured from "../components/Featured";
+import Gallery from "../components/Gallery";
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
       <Inventory />
       <Question />
       <Featured />
+      <Gallery />
     </Fragment>
   );
 };
